3e660688eac82bd489aebff02220c620695032ee
Donglinnn <65762101+Donglinnn@users.noreply.github.com>
2024-07-24 10:05:40 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Che-lun Hu on 2024/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
